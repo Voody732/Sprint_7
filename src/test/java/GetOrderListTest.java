029641beaae48e4f0ba0d4ac9f9498be2bc8fe80
Artemii Petrov <deidara941@yandex.ru>
2025-05-27 06:24:34 +0300

    Tests and add allure report

--- src/test/java/GetOrderListTest.java
+++ src/test/java/GetOrderListTest.java
@@ -1,15 +1,14 @@
-import io.qameta.allure.*;
+import io.qameta.allure.Description;
+import io.qameta.allure.Epic;
+import io.qameta.allure.Feature;
+import io.qameta.allure.Link;
 import io.qameta.allure.junit4.DisplayName;
 import io.qameta.allure.junit4.Tag;
-import io.restassured.response.ValidatableResponse;
 import org.junit.Test;
 import ru.yandex.praktikum.steps.OrderSteps;
 
-import static io.restassured.RestAssured.given;
 import static org.apache.http.HttpStatus.SC_OK;
 import static org.hamcrest.CoreMatchers.notNullValue;
-import static ru.yandex.praktikum.StaticUrls.BASEURL;
-import static ru.yandex.praktikum.StaticUrls.ORDER_LIST_HANDLER;
 
 @Link(url = "https://qa-scooter.praktikum-services.ru/docs/#api-v1-orders", name = "#api/v1/orders")
 @Tag("login-courier")
@@ -23,11 +22,8 @@ public class GetOrderListTest {
     @DisplayName("Получение списка заказов")
     @Description("Тест проверяет API получения списка заказов. ОР - 200 и тела заказов ")
     public void getOrderList() {
-        ValidatableResponse response = given()
-                .header("Content-type", "application/json")
-                .baseUri(BASEURL)
-                .get(ORDER_LIST_HANDLER)
-                .then()
+        orderSteps
+                .getOrderList()
                 .statusCode(SC_OK)
                 .body("orders", notNullValue());
     }
